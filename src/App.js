cd7a79d1d593fd2ff90a19ef071779e474528314
Лазовий Олег Вікторович <LazovoyOV@comfy.ua>
2021-11-12 11:37:29 +0200

    верстка 12112021_11

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import './App.css';
 import React from "react";
-import Header from "./components/Header";
-import Technologies from "./components/Technologies";
+import Header from "./views/Header";
+import Technologies from "./views/Technologies";
+import Footer from "./views/Footer";
 
 //function App() {
 const App = () => {
   return (
-    <div className="App">
-        <Header/>
-        <Technologies />
-    </div>
+      <div className='app-wrapper'>
+          <header className='header'>
+              <img
+                  src='https://st2.depositphotos.com/1064024/10769/i/600/depositphotos_107694484-stock-photo-little-boy.jpg'/>
+              <Header/>
+          </header>
+          <nav className='nav'>
+              <div>Profile</div>
+              <div>Messages</div>
+              <div>Main content</div>
+          </nav>
+
+          <div className='content'>
+              <div>
+                  <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQIeCxX7mFVg3phbJYG5Ho6dR_apaGg5wjbg&usqp=CAU' />
+              </div>
+              <div>
+                  ava + description
+              </div>
+              <div>
+                  My Posts
+                  <div>
+                      New post
+                  </div>
+                  <div>
+                      <div>post 1</div>
+                      <div>post 2</div>
+                      <div>post 3</div>
+                  </div>
+              </div>
+              <Technologies/>
+          </div>
+
+          <footer className='footer'><Footer/></footer>
+      </div>
   );
 }
 
