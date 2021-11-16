f7f0c73c0b0a7d87c69225afa2da37985fc66f46
Лазовий Олег Вікторович <LazovoyOV@comfy.ua>
2021-11-16 17:43:07 +0200

    dialogs 16112021_21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import s from './Dialogs.module.css'
+
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active } >
+                    Dymych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Oleh
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valery
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are your</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 }
